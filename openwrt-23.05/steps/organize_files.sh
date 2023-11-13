0f98c1c74add36d35b8a863d6474fc471970473e
danielpinto8zz6 <danielpinto8zz6@gmail.com>
2023-11-13 11:39:15 +0000

    clean for r4s

--- openwrt-23.05/steps/organize_files.sh
+++ openwrt-23.05/steps/organize_files.sh
@@ -6,19 +6,15 @@ if [ ! -e "$ROOTDIR/build" ]; then
     exit 1
 fi
 
-
-NANOPI_MODEL=$1
-NANOPI_MODEL_LOW=$(echo ${NANOPI_MODEL} | tr '[:upper:]' '[:lower:]')
-VARIANT=$2
-OPENWRT_BRANCH=$3
-RELTAG=$4
+OPENWRT_BRANCH=$1
+RELTAG=$2
 
 mkdir -p ./artifact/
 mv build/openwrt/bin/targets/rockchip/armv8/*sysupgrade.img* ./artifact/
 cd ./artifact/
 ls -Ahl
-mv openwrt-rockchip-armv8-friendlyarm_nanopi-$NANOPI_MODEL_LOW-ext4-sysupgrade.img.gz OpenWrt-AO-NanoPi$NANOPI_MODEL-$VARIANT-$OPENWRT_BRANCH-$RELTAG-ext4.img.gz
-mv openwrt-rockchip-armv8-friendlyarm_nanopi-$NANOPI_MODEL_LOW-squashfs-sysupgrade.img.gz OpenWrt-AO-NanoPi$NANOPI_MODEL-$VARIANT-$OPENWRT_BRANCH-$RELTAG-squashfs.img.gz
+mv openwrt-rockchip-armv8-friendlyarm_nanopi-r4s-ext4-sysupgrade.img.gz OpenWrt-AO-NanoPiR4S-$OPENWRT_BRANCH-$RELTAG-ext4.img.gz
+mv openwrt-rockchip-armv8-friendlyarm_nanopi-r4s-squashfs-sysupgrade.img.gz OpenWrt-AO-NanoPiR4S-$OPENWRT_BRANCH-$RELTAG-squashfs.img.gz
 gzip -d *.gz && exit 0
 gzip --best *.img
 ls -Ahl
